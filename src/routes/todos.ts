1cabdf978cb8a5e4f00bdbef2c7efe7fdf6bdd78
san38700 <sandeepkratosj@yahoo.in>
2024-03-02 15:17:11 +0530

    restructuring code using outdir: '/dist' rootDir to './src' and enabling npm start

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -3,6 +3,9 @@ import { Router } from "express"
 import { Todo } from "../models/todo"
 import { text } from "body-parser"
 
+type RequestBody = {text : string}
+type RequestParams = {todoId : string}
+
 let todos : Todo[] = []
 
 const router =  Router()
@@ -12,9 +15,10 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody
     const newTodo : Todo = {
         id : new Date().toISOString(),
-        text : req.body.text
+        text : body.text
     }
     console.log(newTodo.text)
     todos.push(newTodo)
@@ -22,17 +26,20 @@ router.post('/todo', (req, res, next) => {
 })
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId
+    const body = req.body as RequestBody
+    const params = req.params as RequestParams
+    const tid = params.todoId
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid)
     if (todoIndex >= 0){
-        todos[todoIndex] = { id: todos[todoIndex].id, text : req.body.text}
+        todos[todoIndex] = { id: todos[todoIndex].id, text : body.text}
         return res.status(200).json({message : "Updated Todo", todos : todos})
     }
     res.status(404).json({message: 'Could  not find todo for this id'})
 })
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId)
+    const params = req.params as RequestParams
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId)
     res.status(200).json({message: 'Deleted Todo', todos: todos})
 
 })
